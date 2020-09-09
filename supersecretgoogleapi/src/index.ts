73938707bcff2394aac51e08414669ba63a6bac6
Mykhailo <mishamoroz5@gmail.com>
2020-09-09 21:38:44 +0300

    feat: reviews endpoint

--- supersecretgoogleapi/src/index.ts
+++ supersecretgoogleapi/src/index.ts
@@ -38,7 +38,7 @@ app.get('/locations', async (req, res) => {
   select('id', 'createdAt', 'regionCode', 'languageCode',
     'postalCode', 'sortingCode', 'administrativeArea', 'locality',
     'sublocality', 'addressLines', 'recipients', 'organization').paginate({
-    perPage: 10,
+    perPage: 5,
     currentPage: page,
     isLengthAware: true,
   });
@@ -56,38 +56,29 @@ app.get('/locations', async (req, res) => {
 app.get('/reviews', async (req, res) => {
   const page = Number(req.query.page) || 0;
   const locationId = req.query.locationId
-  const accountId = req.headers['account-id'];
 
-  if(!accountId) {
-    return res.status(400).json({
-      error: 'No account id provided',
-    })
-  }
-  
   if(!locationId) {
     return res.status(400).json({
       error: 'No location id provided',
     })
   }
 
-  const locations = await pg('Accounts_Locations').where({
-    accountId: accountId
-  }).join('Locations', `"Accounts_Locations"."locationId"`, '=', '"Locations".id').
-  select('id', 'createdAt', 'regionCode', 'languageCode',
-    'postalCode', 'sortingCode', 'administrativeArea', 'locality',
-    'sublocality', 'addressLines', 'recipients', 'organization').paginate({
-    perPage: 10,
+  const reviews = await pg('Reviews').where({
+    locationId,
+  }).select('id', 'locationId', 'reviewer', 'starRating',
+    'comment', 'createTime', 'updateTime', 'reviewReply').paginate({
+    perPage: 5,
     currentPage: page,
     isLengthAware: true,
   });
 
   return res.json({
     ...(
-      locations.pagination.lastPage === page ?
-      { nextPage: `/locations?page=${page + 1}` } :
+      reviews.pagination.lastPage === page ?
+      { nextPage: `/reviews?page=${page + 1}&locationId=${locationId}` } :
       {}
     ),
-    results: locations.data,
+    results: reviews.data,
   });
 })
 
